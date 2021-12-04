82bf6909d480a30813afb13d92345349e9d315c4
Chris11CH <chriscurtolo@gmail.com>
2021-12-04 18:00:09 +0100

    Finished info and add obj layout and started AddObjFrag

--- app/src/main/java/com/cyberbug/view/AddObjFrag.java
+++ app/src/main/java/com/cyberbug/view/AddObjFrag.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.grafica.R;
 
@@ -19,12 +20,11 @@ public class AddObjFrag extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_ERROR_MESSAGE = "errorMessage";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText objName;
+    private EditText objDesc;
 
     public AddObjFrag() {
         // Required empty public constructor
@@ -35,26 +35,23 @@ public class AddObjFrag extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AddObjFrag.
      */
     // TODO: Rename and change types and number of parameters
-    public static AddObjFrag newInstance(String param1, String param2) {
-        AddObjFrag fragment = new AddObjFrag();
+    public static AddObjFrag newInstance(String param1) {
+        AddObjFrag aof = new AddObjFrag();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putString(ARG_ERROR_MESSAGE, param1);
+        aof.setArguments(args);
+        return aof;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        /*if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_ERROR_MESSAGE);
+        }*/
     }
 
     @Override
