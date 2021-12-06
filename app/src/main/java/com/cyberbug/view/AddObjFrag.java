eb31a98d4ed7f9c7ba8a8fbbbeb6d5979e89bf3d
Chris11CH <chriscurtolo@gmail.com>
2021-12-06 11:48:30 +0100

    Fixed some ids and strings, continued AddObjFrag and InfoObjFrag (till onCreateView)

--- app/src/main/java/com/cyberbug/view/AddObjFrag.java
+++ app/src/main/java/com/cyberbug/view/AddObjFrag.java
@@ -2,14 +2,23 @@ package com.cyberbug.view;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
+import com.cyberbug.api.APIRequest;
+import com.cyberbug.api.AsyncRESTDispatcher;
+import com.cyberbug.api.FSAPIWrapper;
+import com.cyberbug.api.UIUpdaterResponse;
+import com.cyberbug.api.UIUpdaterVoid;
 import com.example.grafica.R;
+import com.google.android.material.snackbar.Snackbar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,11 +27,10 @@ import com.example.grafica.R;
  */
 public class AddObjFrag extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_ERROR_MESSAGE = "errorMessage";
+    private String errorMessage = null;
 
-    // TODO: Rename and change types of parameters
     private EditText objName;
     private EditText objDesc;
 
@@ -34,14 +42,14 @@ public class AddObjFrag extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
+     * @param errorMessage Parameter 1.
      * @return A new instance of fragment AddObjFrag.
      */
     // TODO: Rename and change types and number of parameters
-    public static AddObjFrag newInstance(String param1) {
+    public static AddObjFrag newInstance(String errorMessage) {
         AddObjFrag aof = new AddObjFrag();
         Bundle args = new Bundle();
-        args.putString(ARG_ERROR_MESSAGE, param1);
+        args.putString(ARG_ERROR_MESSAGE, errorMessage);
         aof.setArguments(args);
         return aof;
     }
@@ -49,15 +57,63 @@ public class AddObjFrag extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_ERROR_MESSAGE);
-        }*/
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_obj, container, false);
+
+        // Check for args
+        if (getArguments() != null) {
+            errorMessage = getArguments().getString(ARG_ERROR_MESSAGE);
+        }
+        // Create fragment view
+        View v = inflater.inflate(R.layout.fragment_add_obj, container, false);
+
+        // Initialize fragment components
+        objName = v.findViewById(R.id.txt_obj_name);
+        objDesc = v.findViewById(R.id.txt_obj_desc);
+
+        // Set buttons listeners
+        Button cancObj = v.findViewById(R.id.btn_canc_obj_add);
+        Button addObj = v.findViewById(R.id.btn_obj_add);
+
+        // Set fragment title
+        Fragment parent = this.getParentFragment();
+        if (parent != null) {
+            // Check to avoid crash if the parent fragment does not exists (should not happen)
+            Toolbar tBar = parent.requireView().findViewById(R.id.toolbar);
+            tBar.setTitle(getString(R.string.add_obj));
+            parent.setHasOptionsMenu(false);
+        }
+
+        return v;
+    }
+
+    public void onClickCancelButton( View v){
+        // Clear the TextViews
+        objName.getText().clear();
+        objDesc.getText().clear();
+    }
+
+    public void onClickAddObjButton( View v){
+        // Get text
+        String name = objName.getText().toString();
+        String desc = objDesc.getText().toString();
+
+        // Error handling
+        if(name.isEmpty() || desc.isEmpty()) {
+            Snackbar.make(this.requireView(), getString(R.string.insert_required_fields), Snackbar.LENGTH_LONG).show();
+            return;
+        }
+
+        // All is ok
+        // Send the request to the serve
+        /*FSAPIWrapper.NewGroupInfo group = new FSAPIWrapper.NewGroupInfo(description, location, name, "true", MainActivity.sData.thisUserId, null, null);
+        UIUpdaterVoid<FragmentActivity> preUpdater = new UIUpdaterVoid<>(this.requireActivity(), CreateGroupFrag::onPreCreateGroupRequest);
+        UIUpdaterResponse<FragmentActivity> postUpdater = new UIUpdaterResponse<>(this.requireActivity(), this::onPostCreateGroupRequest);
+        APIRequest req = MainActivity.fsAPI.createGroupRequest(MainActivity.sData.authToken, group);
+        new AsyncRESTDispatcher(preUpdater, postUpdater).execute(req);*/
+
     }
 }
--- app/src/main/java/com/cyberbug/view/InfoObjFrag.java
+++ app/src/main/java/com/cyberbug/view/InfoObjFrag.java
@@ -2,13 +2,19 @@ package com.cyberbug.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.grafica.R;
+
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,14 +23,14 @@ import com.example.grafica.R;
  */
 public class InfoObjFrag extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_ERROR_MESSAGE = "errorMessage";
+    private String errorMessage = null;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView owner;
+    private TextView desc;
+    private TextView state;
+    private TextView sharedGroup;
 
     public InfoObjFrag() {
         // Required empty public constructor
@@ -34,33 +40,56 @@ public class InfoObjFrag extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param errorMessage Parameter 1.
      * @return A new instance of fragment InfoObjFrag.
      */
     // TODO: Rename and change types and number of parameters
-    public static InfoObjFrag newInstance(String param1, String param2) {
-        InfoObjFrag fragment = new InfoObjFrag();
+    public static InfoObjFrag newInstance(String errorMessage) {
+        InfoObjFrag iof = new InfoObjFrag();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putString(ARG_ERROR_MESSAGE, errorMessage);
+        iof.setArguments(args);
+        return iof;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_obj, container, false);
+
+        // Check for args
+        if (getArguments() != null) {
+            errorMessage = getArguments().getString(ARG_ERROR_MESSAGE);
+        }
+
+        // Create fragment view
+        View v = inflater.inflate(R.layout.fragment_info_obj, container, false);
+
+        // Initialize fragment components
+        owner = v.findViewById(R.id.txt_obj_owner);
+        desc = v.findViewById(R.id.txt_obj_desc);
+        state = v.findViewById(R.id.txt_obj_state);
+        sharedGroup = v.findViewById(R.id.txt_obj_group);
+
+        // Set buttons listeners
+        Button abort = v.findViewById(R.id.btn_abort_loan);
+        Button loan = v.findViewById(R.id.btn_obj_loan);
+
+        // Set fragment title
+        Fragment parent = this.getParentFragment();
+        if (parent != null) {
+            // Check to avoid crash if the parent fragment does not exists (should not happen)
+            Toolbar tBar = parent.requireView().findViewById(R.id.toolbar);
+            tBar.setTitle(getString(R.string.obj_info));
+            parent.setHasOptionsMenu(false);
+        }
+
+        return v;
+
     }
+
 }
