e79a9ebc1d92a052ec5d66ace3ed074d9c2cb5cc
GieMMe-98 <84325707+GieMMe-98@users.noreply.github.com>
2021-12-15 03:50:54 +0100

    Proceeded with RequestObjFrag, RequestsFrag and fragment_request_obj.xml

--- app/src/main/java/com/cyberbug/view/RequestObjFrag.java
+++ app/src/main/java/com/cyberbug/view/RequestObjFrag.java
@@ -2,13 +2,23 @@ package com.cyberbug.view;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.cyberbug.R;
+import com.cyberbug.api.APIRequest;
+import com.cyberbug.api.AsyncRESTDispatcher;
+import com.cyberbug.api.FSAPIWrapper;
+import com.cyberbug.api.UIUpdaterResponse;
+import com.cyberbug.api.UIUpdaterVoid;
+import com.google.android.material.snackbar.Snackbar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,14 +27,14 @@ import com.cyberbug.R;
  */
 public class RequestObjFrag extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_ERROR_MESSAGE = "errorMessage";
+    private String errorMessage = null;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView objOwner;
+    private TextView objDecription;
+    private TextView returnData;
+
 
     public RequestObjFrag() {
         // Required empty public constructor
@@ -34,33 +44,62 @@ public class RequestObjFrag extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment RequestObjFrag.
      */
-    // TODO: Rename and change types and number of parameters
-    public static RequestObjFrag newInstance(String param1, String param2) {
-        RequestObjFrag fragment = new RequestObjFrag();
+
+    public static RequestObjFrag newInstance(String errorMessage) {
+        RequestObjFrag iof = new RequestObjFrag();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putString(ARG_ERROR_MESSAGE, errorMessage);
+        iof.setArguments(args);
+        return iof;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_request_obj, container, false);
+        // Check for args
+        if (getArguments() != null) {
+            errorMessage = getArguments().getString(ARG_ERROR_MESSAGE);
+        }
+
+        // Create a fragment view
+        View v = inflater.inflate(R.layout.fragment_request_obj, container, false);
+
+        // Initialize fragment components
+        objOwner = v.findViewById(R.id.owner_obj_request);
+        objDecription = v.findViewById(R.id.descr_obj_request);
+        returnData = v.findViewById(R.id.return_date_obj_request);
+
+        // Set buttons listeners
+        Button cancelRequest = v.findViewById(R.id.btn_canc_requestObj);
+        cancelRequest.setOnClickListener(this::onClickCancelButton);
+        Button confirmRequest = v.findViewById(R.id.btn_confirm_requestObj);
+        confirmRequest.setOnClickListener(this::onClickAddRequestObjButton);
+
+        // Set fragment title
+        Fragment parent = this.getParentFragment();
+        if (parent != null) {
+            // Check to avoid crash if the parent fragment does not exists (should not happen)
+            Toolbar tBar = parent.requireView().findViewById(R.id.toolbar);
+            tBar.setTitle(getString(R.string.request_an_object));
+            parent.setHasOptionsMenu(false);
+        }
+
+        return v;
+    }
+
+
+    public void onClickCancelButton(View v){
+    }
+
+    public void onClickAddRequestObjButton(View v){
+        // Get text
+
     }
 }
